b5bbfa2ea2243d383db314af15f78c2bb5b81e5b
RahidBot <139769340+RahidBot@users.noreply.github.com>
2023-09-06 15:29:39 +0400

    Delete kelime_bot/helper directory

--- kelime_bot/helper/keyboards.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("📢 Rəsmi Kanal" , url= "ElikoResmi")]
-])
-
-
