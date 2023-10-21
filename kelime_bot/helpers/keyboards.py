bb72d8c1de63f83dd215b5b68a936e94dfff108b
ElikoAndMee <legendmuzkao99@gmail.com>
2023-10-21 20:35:45 +0400

    Update keyboards.py

--- kelime_bot/helpers/keyboards.py
+++ kelime_bot/helpers/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("Status Kanal🕊️" , url= "t.me/adelina302")]
+    [InlineKeyboardButton("Status Kanal 🕊️" , url= "t.me/adelina302")]
 ])
 
 
